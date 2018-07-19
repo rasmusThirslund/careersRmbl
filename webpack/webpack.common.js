b8002447933f95ec0b811b8efd712927ff02a1a7
Rasmus Thirslund <rasmus.thirslund@implicit.dk>
2018-07-19 14:31:51 +0200

    webpack extended with a separate subpages configuration file

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -1,16 +1,19 @@
 'use strict';
 
+const merge = require('webpack-merge');
 const Path = require('path');
+const subpages = require('./webpack.subpages.mergable.js');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const Webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const PATH_DEST = Path.join(__dirname, '../dist');
-const PATH_HTML = Path.join(__dirname, '../public/html');
+// const PATH_HTML = Path.join(__dirname, '../public/html');
 const PATH_SRC = Path.join(__dirname, '../src');
 
-module.exports = {
+module.exports = merge(subpages, {
+
     entry: [
         Path.resolve(__dirname, './polyfills'),
         Path.resolve(__dirname, PATH_SRC + '/scripts/index')
@@ -21,35 +24,32 @@ module.exports = {
     },
     plugins: [
         new CleanWebpackPlugin([PATH_DEST]),
-        //   {
-        //     root: __dirname + '/' + '../'
-        // }),
         new CopyWebpackPlugin([
             {from: Path.resolve(__dirname, '../public'), to: 'public'}
         ]),
         new HtmlWebpackPlugin({
             template: Path.resolve(__dirname, PATH_SRC + '/index.html')
         }),
-        new HtmlWebpackPlugin({
-            filename: 'components.html',
-            template: Path.resolve(__dirname, PATH_HTML + '/components.html')
-        }),
-        new HtmlWebpackPlugin({
-            filename: 'objects.html',
-            template: Path.resolve(__dirname, PATH_HTML + '/objects.html')
-        }),
-        new HtmlWebpackPlugin({
-            filename: 'objects-framework.html',
-            template: Path.resolve(__dirname, PATH_HTML + '/objects-framework.html')
-        }),
-        new HtmlWebpackPlugin({
-            filename: 'heroes.html',
-            template: Path.resolve(__dirname, PATH_HTML + '/heroes.html')
-        }),
-        new HtmlWebpackPlugin({
-            filename: 'frontpage-search.html',
-            template: Path.resolve(__dirname, PATH_HTML + '/frontpage-search.html')
-        }),
+        // new HtmlWebpackPlugin({
+        //     filename: 'components.html',
+        //     template: Path.resolve(__dirname, PATH_HTML + '/components.html')
+        // }),
+        // new HtmlWebpackPlugin({
+        //     filename: 'objects.html',
+        //     template: Path.resolve(__dirname, PATH_HTML + '/objects.html')
+        // }),
+        // new HtmlWebpackPlugin({
+        //     filename: 'objects-framework.html',
+        //     template: Path.resolve(__dirname, PATH_HTML + '/objects-framework.html')
+        // }),
+        // new HtmlWebpackPlugin({
+        //     filename: 'heroes.html',
+        //     template: Path.resolve(__dirname, PATH_HTML + '/heroes.html')
+        // }),
+        // new HtmlWebpackPlugin({
+        //     filename: 'frontpage-search.html',
+        //     template: Path.resolve(__dirname, PATH_HTML + '/frontpage-search.html')
+        // }),
 
     ],
     resolve: {
@@ -70,4 +70,4 @@ module.exports = {
             }
         ]
     }
-};
+});
---
+++ webpack/webpack.subpages.mergable.js
@@ -0,0 +1,41 @@
+'use strict';
+
+
+// const merge = require('webpack-merge');
+const Path = require('path');
+// const CleanWebpackPlugin = require('clean-webpack-plugin');
+// const CopyWebpackPlugin = require('copy-webpack-plugin');
+// const Webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+// const PATH_DEST = Path.join(__dirname, '../dist');
+// const PATH_SRC = Path.join(__dirname, '../src');
+const PATH_HTML = Path.join(__dirname, '../public/html');
+
+module.exports = {
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'components.html',
+            template: Path.resolve(__dirname, PATH_HTML + '/components.html')
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'objects.html',
+            template: Path.resolve(__dirname, PATH_HTML + '/objects.html')
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'objects-framework.html',
+            template: Path.resolve(__dirname, PATH_HTML + '/objects-framework.html')
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'heroes.html',
+            template: Path.resolve(__dirname, PATH_HTML + '/heroes.html')
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'frontpage-search.html',
+            template: Path.resolve(__dirname, PATH_HTML + '/frontpage-search.html')
+        }),
+
+    ]
+
+};
+
